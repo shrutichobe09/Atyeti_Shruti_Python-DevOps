6263b77d9a524ee528e9478a6610cc3b372c8555
shrutichobe09 <shruti.chobe@atyeti.com>
2025-05-12 17:41:37 +0530

    my files

---
+++ Python/Fudamentals/data_types.py
@@ -0,0 +1,15 @@
+# Define variables with different data types
+employee_name = "Ram"    #string
+age=27                   #integer
+experience=2.5           #float
+project = ["ecommerce", "food-ordering", "hotel-management"]  #list
+dict = {'developer': 'sham', 'HR':'shreyas','manager':'Devank'}  #dictionary
+bool = True
+
+# Get and print the type of each variable
+print(type(employee_name))   
+print(type(age)) 
+print(type(experience))   
+print(type(project))     
+print(type(dict))     
+print(type(bool))
