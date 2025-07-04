e09a178dba81479e12cb8f9736b97325203b96b7
shrutichobe09 <shruti.chobe@atyeti.com>
2025-07-04 09:22:16 +0530

    Update models.py

--- Devops/GitHubAction/python-task-manager-CICD/app/models.py
+++ Devops/GitHubAction/python-task-manager-CICD/app/models.py
@@ -6,4 +6,4 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String)
-    completed = Column(Boolean, default=False)
+    completed = Column(Boolean, default=False)  
